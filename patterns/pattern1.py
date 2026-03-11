f42cbdfa48bad7d1e2701fb7b0f3ab191e804a6c
sivaraman-dev <sivaramancoder18@gmail.com>
2026-03-11 22:31:16 +0530

    push pattern programs

---
+++ patterns/pattern1.py
@@ -0,0 +1,6 @@
+n = 5 #number of lines
+
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        print("*",end="")
+    print("\n")
---
+++ patterns/pattern3.py
@@ -0,0 +1,5 @@
+n = 5 #number of lines
+for i in range(n,0,-1):
+    for j in range(1,i+1):
+        print("*",end="")
+    print("\n")
