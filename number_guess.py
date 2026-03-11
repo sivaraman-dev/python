60570eddd7ab05f9b55d80a9c3e1e2c3a9319be0
sivaraman-dev <sivaramancoder18@gmail.com>
2026-03-11 21:05:42 +0530

    push number_guess.py

---
+++ number_guess.py
@@ -0,0 +1,19 @@
+import random
+secret_number = random.randint(1, 100)
+print("Welcome to Number Guessing Game!")
+print("Guess a number between 1 and 100")
+attempts = 0
+while True:
+    guess = int(input("Enter your guess: "))
+    attempts += 1
+
+    if guess < secret_number:
+        print("Too low! Try again.")
+
+    elif guess > secret_number:
+        print("Too high! Try again.")
+
+    else:
+        print("Correct! You guessed the number.")
+        print("Total attempts:", attempts)
+        break
